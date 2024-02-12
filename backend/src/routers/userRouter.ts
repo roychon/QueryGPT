c0b6607f5592bd75de46d94d891e2030ee2dac93
Roy Chon <110176807+roychon@users.noreply.github.com>
2024-02-12 16:34:38 -0500

    set up routers

---
+++ backend/src/routers/userRouter.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import { userLogIn, userLogOut, userSignUp } from '../controllers/user.controller.js'
+import { verifyToken } from '../validate/verifyToken.js'
+
+const userRouter  = express.Router()
+
+userRouter.post("/signup", userSignUp)
+userRouter.post("/login", userLogIn)
+userRouter.post("/logout", userLogOut)
+userRouter.post("/cookies", userLogIn, verifyToken)
+
+export default userRouter
