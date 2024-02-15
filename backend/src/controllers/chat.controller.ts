c94af28cdabe9416b373a127a14d149fc6c991ca
Roy Chon <110176807+roychon@users.noreply.github.com>
2024-02-15 00:25:20 -0500

    add functionality to add + delete thread, edit thread title

--- backend/src/controllers/chat.controller.ts
+++ backend/src/controllers/chat.controller.ts
@@ -1,7 +1,7 @@
 import { ChatOpenAI } from "@langchain/openai"
 import { Request, Response, NextFunction } from "express"
 import { ConversationSummaryBufferMemory } from "langchain/memory";
-import {User, ConversationPair, Chat} from "../model/User.js"
+import {User, ConversationPair, Chat, Thread} from "../model/User.js"
 import { ConversationChain } from "langchain/chains";
 import { rmSync } from "fs";
 
@@ -67,6 +67,49 @@ export const getUserChats = async (req: Request, res: Response, next: NextFuncti
     try {
         const chats = await getSortedUserChats(res.locals.user.username)
         return res.status(201).json(chats)
+    } catch (e) {
+        return res.status(401).send(e.message)
+    }
+}
+
+// rename thread
+export const renameThread = async (req: Request, res: Response, next: NextFunction) => {
+    const { title, threadId } = req.body
+    try {
+        // TODO: create a function to get thread by specific threadId (which is ObjectId)
+        const user = await User.findOne({username: res.locals.user.username})
+        const thread = user.threads.find(thread => thread._id.toString() == threadId)
+        thread.title=  title
+        await user.save()
+        return res.status(201).send("successfully renamed thread title")
+    } catch (e) {
+        return res.status(401).send(e.message)
+    }
+}
+
+// delete a thread
+export const deleteThread = async (req: Request, res: Response, next: NextFunction) => {
+    const { threadId } = req.body
+    try {
+        // TODO: create a function to get thread by specific threadId (which is ObjectId)
+        const user = await User.findOne({username: res.locals.user.username})
+        user.threads.filter(thread => thread._id.toString() !== threadId)
+        await user.save()
+        return res.status(201).send(`successfully deleted thread ${threadId}`)
+    } catch (e) {
+        return res.status(401).send(e.message)
+    }
+}
+
+// add a thread
+export const addThread = async (req: Request, res: Response, next: NextFunction) => {
+    const { threadId } = req.body
+    try {
+        const user = await User.findOne({username: res.locals.user.username}) // TODO: makes this a method in model
+        const newThread = new Thread()
+        user.threads.push(newThread)
+        await user.save()
+        return res.status(201).send("successfully added new thread")
     } catch (e) {
         return res.status(401).send(e.message)
     }
