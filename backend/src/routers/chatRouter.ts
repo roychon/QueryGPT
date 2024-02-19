4dc13c7474a378cf15d0ce894a5b6572ec932d4f
Roy Chon <110176807+roychon@users.noreply.github.com>
2024-02-18 21:15:43 -0500

    set new router for creating new thread + getting chats from specific thread

--- backend/src/routers/chatRouter.ts
+++ backend/src/routers/chatRouter.ts
@@ -1,13 +1,14 @@
 import { Router} from "express"
-import { deleteThread, generateAIResponse, getUserChats, renameThread } from "../controllers/chat.controller.js"
+import { createNewThread, deleteThread, generateAIResponse, getUserChats, renameThread } from "../controllers/chat.controller.js"
 import { verifyToken } from "../validate/verifyToken.js"
 
 const chatRouter = Router()
 
 chatRouter.post("/", verifyToken, generateAIResponse)
 chatRouter.put("/", verifyToken, generateAIResponse) // edit: just send newly edited message as entirely new message
-chatRouter.get("/", verifyToken, getUserChats)
+chatRouter.post("/getUserChats", verifyToken, getUserChats)
 chatRouter.put("/rename-thread", verifyToken, renameThread)
 chatRouter.delete("/delete-thread", verifyToken, deleteThread)
+chatRouter.post("/thread", verifyToken, createNewThread)
 
 export default chatRouter
