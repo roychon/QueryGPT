a788545188d864a4cec5bb513d1c379a5d15fc2b
Roy Chon <110176807+roychon@users.noreply.github.com>
2024-02-18 21:16:33 -0500

    add Icon component for mic + folder icon

---
+++ frontend/src/components/Icons.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { IoIosSend } from "react-icons/io";
+import { AiOutlineAudio } from "react-icons/ai";
+
+export default function Icon({text}: {text: String}) {
+    return (
+        <p style={{padding: "1px 15px", outline: "none", border: "1px solid white", display: "flex", justifyContent: "center", alignItems: "center", borderRadius: "15px"}}>
+            {text == "Audio" ? <AiOutlineAudio /> : <IoIosSend />}
+        </p>
+    )
+}
