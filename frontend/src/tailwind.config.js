b86f029c03b42bf57296b96b47aa293482ea2e91
Roy Chon <110176807+roychon@users.noreply.github.com>
2024-02-26 15:05:09 -0500

    configure tailwind.config.js custom colors

--- frontend/src/tailwind.config.js
+++ frontend/src/tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  mode: 'jit',
-  content: [
-    "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: "jit",
+  content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         "dark-purple": "#01063C",
         "medium-purple": "#C28EFA",
-        "bright-blue": "#1033F5"
-      }
+        "bright-blue": "#1033F5",
+        "background-gray": "#313030",
+        "background-dark-gray": "#181818",
+        "bright-purple": "#534891",
+      },
     },
   },
   plugins: [],
-}
-
+};
